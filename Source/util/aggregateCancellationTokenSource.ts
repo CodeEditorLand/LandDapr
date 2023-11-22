ea7f096f69bcec23f5b9aa92c35b7b5d90ea51ad
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-22 10:38:52 +0200

    squash!

--- Source/util/aggregateCancellationTokenSource.ts
+++ Source/util/aggregateCancellationTokenSource.ts
@@ -1,31 +1,38 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-export async function withAggregateTokens<T>(callback: (token: vscode.CancellationToken) => Promise<T>, ...tokens: (vscode.CancellationToken | undefined)[]): Promise<T> {
-    const cancellationTokenSource = new AggregateCancellationTokenSource(...tokens);
-    try {
-        return await callback(cancellationTokenSource.token);
-    } finally {
-        cancellationTokenSource.dispose();
-    }
+export async function withAggregateTokens<T>(
+	callback: (token: vscode.CancellationToken) => Promise<T>,
+	...tokens: (vscode.CancellationToken | undefined)[]
+): Promise<T> {
+	const cancellationTokenSource = new AggregateCancellationTokenSource(
+		...tokens
+	);
+	try {
+		return await callback(cancellationTokenSource.token);
+	} finally {
+		cancellationTokenSource.dispose();
+	}
 }
 
 export default class AggregateCancellationTokenSource extends vscode.CancellationTokenSource {
-    private readonly listeners: (vscode.Disposable | undefined)[];
+	private readonly listeners: (vscode.Disposable | undefined)[];
 
-    constructor(...tokens: (vscode.CancellationToken | undefined)[]) {
-        super();
+	constructor(...tokens: (vscode.CancellationToken | undefined)[]) {
+		super();
 
-        this.listeners = tokens.map(token => token?.onCancellationRequested(() => this.cancel()));
-    }
+		this.listeners = tokens.map(
+			(token) => token?.onCancellationRequested(() => this.cancel())
+		);
+	}
 
-    dispose() {
-        for (const listener of this.listeners) {
-            listener?.dispose();
-        }
+	dispose() {
+		for (const listener of this.listeners) {
+			listener?.dispose();
+		}
 
-        super.dispose();
-    }
+		super.dispose();
+	}
 }
--- Source/views/applications/daprApplicationTreeDataProvider.ts
+++ Source/views/applications/daprApplicationTreeDataProvider.ts
@@ -2,110 +2,163 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import * as path from 'path';
-import * as vscode from 'vscode';
-import { DaprApplication, DaprApplicationProvider } from '../../services/daprApplicationProvider';
-import TreeNode from '../treeNode';
-import { DaprInstallationManager } from '../../services/daprInstallationManager';
-import { UserInput } from '../../services/userInput';
-import { DaprClient } from '../../services/daprClient';
-import { Subscription } from 'rxjs';
-import { DaprRunNode } from './daprRunNode';
-import DaprApplicationNode from './daprApplicationNode';
+import * as path from "path";
+import * as vscode from "vscode";
+import {
+	DaprApplication,
+	DaprApplicationProvider,
+} from "../../services/daprApplicationProvider";
+import TreeNode from "../treeNode";
+import { DaprInstallationManager } from "../../services/daprInstallationManager";
+import { UserInput } from "../../services/userInput";
+import { DaprClient } from "../../services/daprClient";
+import { Subscription } from "rxjs";
+import { DaprRunNode } from "./daprRunNode";
+import DaprApplicationNode from "./daprApplicationNode";
 
-export default class DaprApplicationTreeDataProvider extends vscode.Disposable implements vscode.TreeDataProvider<TreeNode> {
-    private readonly onDidChangeTreeDataEmitter = new vscode.EventEmitter<TreeNode | null | undefined>();
-    private readonly applicationProviderListener: Subscription;
-    private applications: DaprApplication[] = [];
+export default class DaprApplicationTreeDataProvider
+	extends vscode.Disposable
+	implements vscode.TreeDataProvider<TreeNode>
+{
+	private readonly onDidChangeTreeDataEmitter = new vscode.EventEmitter<
+		TreeNode | null | undefined
+	>();
+	private readonly applicationProviderListener: Subscription;
+	private applications: DaprApplication[] = [];
 
-    constructor(
-        private readonly applicationProvider: DaprApplicationProvider,
-        private readonly daprClient: DaprClient,
-        private readonly installationManager: DaprInstallationManager,
-        private readonly ui: UserInput) {
-        super(() => {
-            this.applicationProviderListener.unsubscribe();
-            this.onDidChangeTreeDataEmitter.dispose();
-        });
+	constructor(
+		private readonly applicationProvider: DaprApplicationProvider,
+		private readonly daprClient: DaprClient,
+		private readonly installationManager: DaprInstallationManager,
+		private readonly ui: UserInput
+	) {
+		super(() => {
+			this.applicationProviderListener.unsubscribe();
+			this.onDidChangeTreeDataEmitter.dispose();
+		});
 
-        this.applicationProviderListener =
-            this.applicationProvider
-                .applications
-                .subscribe(
-                    applications => {
-                        this.applications = applications;
-                        this.onDidChangeTreeDataEmitter.fire(undefined);
-                    });
-    }
+		this.applicationProviderListener =
+			this.applicationProvider.applications.subscribe((applications) => {
+				this.applications = applications;
+				this.onDidChangeTreeDataEmitter.fire(undefined);
+			});
+	}
 
-    get onDidChangeTreeData(): vscode.Event<TreeNode | null | undefined> | undefined {
-        return this.onDidChangeTreeDataEmitter.event;
-    }
-    
-    getTreeItem(element: TreeNode): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        return element.getTreeItem();
-    }
+	get onDidChangeTreeData():
+		| vscode.Event<TreeNode | null | undefined>
+		| undefined {
+		return this.onDidChangeTreeDataEmitter.event;
+	}
 
-    async getChildren(element?: TreeNode): Promise<TreeNode[]> {
-        if (element) {
-            return element.getChildren?.() ?? [];
-        } else {
-            const isInitialized = await this.installationManager.isInitialized();
+	getTreeItem(
+		element: TreeNode
+	): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return element.getTreeItem();
+	}
 
-            if (isInitialized) {
-                await this.ui.executeCommand('setContext', 'vscode-dapr.views.applications.state', 'notRunning');
-            } else {
-                const isInstalled = await this.installationManager.isInstalled();
-    
-                if (isInstalled) {
-                    await this.ui.executeCommand('setContext', 'vscode-dapr.views.applications.state', 'notInitialized');
-                } else {
-                    await this.ui.executeCommand('setContext', 'vscode-dapr.views.applications.state', 'notInstalled');
-                }
-            }
-    
-            const runs = this.getRuns();
+	async getChildren(element?: TreeNode): Promise<TreeNode[]> {
+		if (element) {
+			return element.getChildren?.() ?? [];
+		} else {
+			const isInitialized =
+				await this.installationManager.isInitialized();
 
-            // NOTE: Returning zero children indicates to VS Code that is should display a "welcome view".
-            //       The one chosen for display depends on the context set above.
+			if (isInitialized) {
+				await this.ui.executeCommand(
+					"setContext",
+					"vscode-dapr.views.applications.state",
+					"notRunning"
+				);
+			} else {
+				const isInstalled =
+					await this.installationManager.isInstalled();
 
-            return runs;
-        }
-    }
+				if (isInstalled) {
+					await this.ui.executeCommand(
+						"setContext",
+						"vscode-dapr.views.applications.state",
+						"notInitialized"
+					);
+				} else {
+					await this.ui.executeCommand(
+						"setContext",
+						"vscode-dapr.views.applications.state",
+						"notInstalled"
+					);
+				}
+			}
 
-    private getRuns(): TreeNode[] {
-        const runs: { [key: string]: { applications: DaprApplication[], runTemplatePath: string } } = {};
-        const individualApps: DaprApplication[] = [];
+			const runs = this.getRuns();
 
-        for (const application of this.applications) {
-            if (application.runTemplatePath) {
-                // TODO: Grouping needs to be done via <PPID, RunTemplatePath> to allow for multiple runs.
-                const name = path.basename(path.dirname(application.runTemplatePath));
+			// NOTE: Returning zero children indicates to VS Code that is should display a "welcome view".
+			//       The one chosen for display depends on the context set above.
 
-                const run = runs[name] ?? { applications: [], runTemplatePath: application.runTemplatePath };
+			return runs;
+		}
+	}
 
-                run.applications.push(application);
+	private getRuns(): TreeNode[] {
+		const runs: {
+			[key: string]: {
+				applications: DaprApplication[];
+				runTemplatePath: string;
+			};
+		} = {};
+		const individualApps: DaprApplication[] = [];
 
-                runs[name] = run;
-            } else {
-                individualApps.push(application);
-            }
-        }
+		for (const application of this.applications) {
+			if (application.runTemplatePath) {
+				// TODO: Grouping needs to be done via <PPID, RunTemplatePath> to allow for multiple runs.
+				const name = path.basename(
+					path.dirname(application.runTemplatePath)
+				);
 
-        const items: TreeNode[] = [];
+				const run = runs[name] ?? {
+					applications: [],
+					runTemplatePath: application.runTemplatePath,
+				};
 
-        const runNames = Object.keys(runs);
+				run.applications.push(application);
 
-        if (runNames.length > 0) {
-            items.push(...runNames.map(name => DaprRunNode.CreateRunNode(name, runs[name].runTemplatePath, runs[name].applications, this.daprClient)))
+				runs[name] = run;
+			} else {
+				individualApps.push(application);
+			}
+		}
 
-            if (individualApps.length > 0) {
-                items.push(DaprRunNode.CreateIndividualApplicationsNode(individualApps, this.daprClient));
-            }
-        } else {
-            items.push(...individualApps.map(application => new DaprApplicationNode(application, this.daprClient)))
-        }
+		const items: TreeNode[] = [];
 
-        return items;
-    }
+		const runNames = Object.keys(runs);
+
+		if (runNames.length > 0) {
+			items.push(
+				...runNames.map((name) =>
+					DaprRunNode.CreateRunNode(
+						name,
+						runs[name].runTemplatePath,
+						runs[name].applications,
+						this.daprClient
+					)
+				)
+			);
+
+			if (individualApps.length > 0) {
+				items.push(
+					DaprRunNode.CreateIndividualApplicationsNode(
+						individualApps,
+						this.daprClient
+					)
+				);
+			}
+		} else {
+			items.push(
+				...individualApps.map(
+					(application) =>
+						new DaprApplicationNode(application, this.daprClient)
+				)
+			);
+		}
+
+		return items;
+	}
 }
